81b29c47543bd58692f52032afbe2e0e1ac9bde9
Andy McVitty <andy.mcvitty@gmail.com>
2022-12-23 00:37:53 +0000

    Refactor

--- day8/lib.py
+++ day8/lib.py
@@ -1,45 +1,38 @@
-def up_score(grid, i, j):
+
+def any_score(grid, i, j, fn):
+    "The function passed as the last arg is the traversal"
     height = grid[i][j]
-    s = 0
-    while i-1 >= 0:
-        i = i-1
-        s += 1
+    visible_trees = 0
+    i, j = fn(i, j)
+    # while we're still on the grid, traverse
+    while i >= 0 and j >= 0 and i < len(grid) and j < len(grid[0]):
+        visible_trees += 1
+        # Stop if this is the last tree
         if grid[i][j] >= height:
             break
-    return s
+        i, j = fn(i, j)
+
+    return visible_trees
+
+
+def up_score(grid, i, j):
+    def go_up(i, j): return i-1, j
+    return any_score(grid, i, j, go_up)
 
 
 def down_score(grid, i, j):
-    height = grid[i][j]
-    s = 0
-    while i+1 < len(grid):
-        i = i+1
-        s += 1
-        if grid[i][j] >= height:
-            break
-    return s
+    def go_down(i, j): return i+1, j
+    return any_score(grid, i, j, go_down)
 
 
 def right_score(grid, i, j):
-    height = grid[i][j]
-    s = 0
-    while j+1 < len(grid[0]):
-        j = j+1
-        s += 1
-        if grid[i][j] >= height:
-            break
-    return s
+    def go_down(i, j): return i, j+1
+    return any_score(grid, i, j, go_down)
 
 
 def left_score(grid, i, j):
-    height = grid[i][j]
-    s = 0
-    while j-1 >= 0:
-        j = j-1
-        s += 1
-        if grid[i][j] >= height:
-            break
-    return s
+    def go_down(i, j): return i, j-1
+    return any_score(grid, i, j, go_down)
 
 
 def score(grid, i, j):
