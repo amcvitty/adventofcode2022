51b81c878555943364b5adcf0f59d31dd9ad37f4
Andy McVitty <andy.mcvitty@gmail.com>
2022-12-24 14:23:02 +0000

    Day10 part2

--- day10/main.py
+++ day10/main.py
@@ -11,7 +11,8 @@ class CPU:
         self.x = 1
         self.cycle = 1
         self.addnext = 0
-        self.xs = [0]
+        self.xs = []
+        self.pixels = ["0"]
 
     def do_tick(self, line):
         if line == "noop":
@@ -23,6 +24,13 @@ class CPU:
             self.x += a
 
     def tick(self):
+        pos = ((self.cycle - 1) % 40)
+        if pos >= self.x - 1 and pos <= self.x + 1:
+            self.pixels.append("#")
+        else:
+            self.pixels.append(".")
+        print(f"c: {self.cycle}, pos: {pos}, sprite: {self.x}")
+
         self.cycle += 1
         self.xs.append(self.x)
 
@@ -33,9 +41,12 @@ for line in lines:
     cpu.do_tick(line)
 
 
+# Part 1 answer
 ss = [cpu.xs[c]*c for c in [20, 60, 100, 140, 180, 220]]
 
 print(ss)
 print(f"Part1: {sum(ss)}")
 
-print(cpu.xs)
+# print(cpu.xs)
+for n in [40, 80, 120, 160, 200, 240]:
+    print("".join(cpu.pixels[n-39:n]))
